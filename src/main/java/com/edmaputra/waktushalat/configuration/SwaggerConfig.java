0afb153043b3a575f61c74ade849b0dba97ae41e
Bangun Edma Saputra <bangun.edma.saputra@gmail.com>
2018-07-28 15:27:33 +0800

    Api Info on Swagger

--- src/main/java/com/edmaputra/waktushalat/configuration/SwaggerConfig.java
+++ src/main/java/com/edmaputra/waktushalat/configuration/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.edmaputra.waktushalat.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,7 +21,19 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.edmaputra.waktushalat.controller"))
                 .paths(regex("/shalat.*"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo("Islam-API",
+                "API untuk yang berhubungan dengan agama Islam khususnya tentang jadwal shalat untuk kota di seluruh dunia",
+                "1.0",
+                "",
+                new Contact("Bangun Edma Saputra", "https://islam.edmaputra.com", "bangun.edma.saputra@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0");
+        return apiInfo;
     }
 
 
